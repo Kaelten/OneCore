61598a374092851d65aa2a4e46649a3da0cfa9e6
Bryan McLemore <kaelten@gmail.com>
2014-10-18 19:11:56 -0500

    Adding support for new WoW Localizations, actually loading the esMX one

---
+++ OneCore-1.0/localization/itIT.lua
@@ -0,0 +1,7 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("OneCore-1.0", "itIT")
+
+if L then
+    --[===[@non-debug@
+    @localization(locale="itIT", format="lua_additive_table")@
+    --@end-non-debug@]===]
+end
